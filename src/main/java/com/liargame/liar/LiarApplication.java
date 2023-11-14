6aa22f76b6841a62cedb12eb4438a2272ebc7a8f
rudrbgus <rudrbgus3@gmail.com>
2023-11-14 18:46:49 +0900

    Initial commit

---
+++ src/main/java/com/liargame/liar/LiarApplication.java
@@ -0,0 +1,13 @@
+package com.liargame.liar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiarApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/liargame/liar/chap01/PracticeController.java
@@ -0,0 +1,7 @@
+package com.liargame.liar.chap01;
+
+import org.springframework.stereotype.Controller;
+
+@Controller()
+public class Practice {
+}
