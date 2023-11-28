c2ac6071e486b63d2e1821fdcda49041dca620a9
rudrbgus <rudrbgus3@gmail.com>
2023-11-28 16:27:45 +0900

    게임 결과 보내기 성공

--- src/main/java/com/liargame/liar/chap01/service/GameService.java
+++ src/main/java/com/liargame/liar/chap01/service/GameService.java
@@ -1,15 +1,13 @@
 package com.liargame.liar.chap01.service;
 
 import com.liargame.liar.chap01.GameChatArray;
+import com.liargame.liar.chap01.Room;
 import com.liargame.liar.chap01.RoomList;
 import com.liargame.liar.chap01.User;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 import static com.liargame.liar.chap01.RoomList.roomList;
 
@@ -85,30 +83,24 @@ public class GameService {
             case 1:
                 return "이제 게임이 시작됩니다";
             case 2:
-
             case 3:
-
             case 4:
             case 5:
             case 6:
             case 7:
             case 8:
             case 9:
-
             case 10:
                 return getUserName(roomCode)+"님 제시어에 대해서 설명해주세요";
             case 11:
                 return "이제 라이어를 지목해주세요!!";
             case 12:
-                return "기다려주세요";
+                return "기다려주세요!";
             case 13:
-                return "기다려주세요";
+                return "기다려주세요!!";
             case 14:
-                return "기다려주세요";
-            case 15:
-                anwser();
-                return "라이어가 이겼습니다!!";
-
+                String anwser1 = anwser();
+                return anwser1;
         }
         return "";
     }
@@ -127,15 +119,71 @@ public class GameService {
         }else{
             System.out.println("이미 유저는 클릭했습니다");
         }
-
     }
-    public void anwser(){
+    public String anwser(){
+        int num1 =0;
+        int num2 = 0;
+        int num3 = 0;
         String anwser1 = anwser.get(RoomList.roomList.get(0).getUsers().get(0).getName());
         String anwser2 = anwser.get(RoomList.roomList.get(0).getUsers().get(1).getName());
-
+        String anwser3 = anwser.get(RoomList.roomList.get(0).getUsers().get(2).getName());
         System.out.println(anwser1);
         System.out.println(anwser2);
+        System.out.println(anwser3);
+        if(anwser1.equals(RoomList.roomList.get(0).getUsers().get(0).getName())){
+            num1++;
+        } else if (anwser1.equals(RoomList.roomList.get(0).getUsers().get(1).getName())){
+            num2++;
+        }else{
+            num3++;
+        }
+        if(anwser2.equals(RoomList.roomList.get(0).getUsers().get(0).getName())){
+            num1++;
+        } else if (anwser2.equals(RoomList.roomList.get(0).getUsers().get(1).getName())){
+            num2++;
+        }else{
+            num3++;
+        }
+        if(anwser3.equals(RoomList.roomList.get(0).getUsers().get(0).getName())){
+            num1++;
+        } else if (anwser3.equals(RoomList.roomList.get(0).getUsers().get(1).getName())) {
+            num2++;
+        }else{
+            num3++;
+        }
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+        if(num1>num2&&num1>num3)
+        {
+            System.out.println("1번 실행");
+            if(role.get(RoomList.roomList.get(0).getUsers().get(0).getName()).equals("라이어")){
+                return RoomList.roomList.get(0).getUsers().get(0).getName()+"님은 라이어 입니다 시민이 이겼습니다";
+            }else{
+                return RoomList.roomList.get(0).getUsers().get(0).getName()+"님은 라이어가 아니였습니다 라이어가 이겼습니다";
+            }
+        }
+        else if(num2>num1&&num2>num3)
+        {
+            System.out.println("2번 실행");
+            if(role.get(RoomList.roomList.get(0).getUsers().get(1).getName()).equals("라이어")){
+                return RoomList.roomList.get(0).getUsers().get(1).getName()+"님은 라이어 입니다 시민이 이겼습니다";
+            }else{
+                return RoomList.roomList.get(0).getUsers().get(1).getName()+"님은 라이어가 아니였습니다 라이어가 이겼습니다";
+            }
+        }
+        else if(num3>num1&&num3>num2){
+            System.out.println("3번 실행");
+            if(role.get(RoomList.roomList.get(0).getUsers().get(2).getName()).equals("라이어")){
+                return RoomList.roomList.get(0).getUsers().get(2).getName()+"님은 라이어 입니다 시민이 이겼습니다";
+            }else{
+                return RoomList.roomList.get(0).getUsers().get(2).getName()+"님은 라이어가 아니였습니다 라이어가 이겼습니다";
+            }
+        }
+        else{
+            return "동표가 나왔습니다 라이어가 이겼습니다";
+        }
     }
-
-
 }
